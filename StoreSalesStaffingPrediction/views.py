a3add7ff6f6334748dfc1552f5a106141f56153f
Ajay Kanubhai Patel <Apatel8978@conestogac.on.ca>
2022-11-17 15:19:18 -0500

    TODO : Ajay - Category insertion and stock detail html design

--- StoreSalesStaffingPrediction/views.py
+++ StoreSalesStaffingPrediction/views.py
@@ -182,5 +182,53 @@ def seasonmaster():
       return render_template('SeasonMaster.html') 
 
 
+@app.route('/category', methods=['POST', 'GET'])
+def Category():
+    if request.method == 'POST':
+        user = request.form
+        user = [request.form['Category']]
+        if user:
+            DRIVER = 'SQL Server'
+            SERVER_NAME = 'BHAGVATI\ASQL'
+            DATABASE_NAME = 'StoreSalesPrediction'
+            cursor = ''
+
+            conn_string = f"""
+              Driver={{{DRIVER}}};
+              Server={SERVER_NAME};
+              Database={DATABASE_NAME};
+              Trust_Connection=yes;
+          """
+
+            try:
+                conn = odbc.connect(conn_string)
+            except Exception as e:
+                print(e)
+                print('task is terminated')
+                sys.exit()
+            else:
+                cursor = conn.cursor()
+
+                insert_statement = """
+                INSERT INTO CategoryMaster
+                VALUES (?)
+              """
+
+                try:
+                    cursor.execute(insert_statement, user)
+                except Exception as e:
+                    cursor.rollback()
+                    print(e.value)
+                    print('transaction rolled back')
+                else:
+                    print('registrated successfully')
+                    cursor.commit()
+                    cursor.close()
+
+                    return render_template('category.html', category=user)
+    else:
+        return render_template('Category.html')
+
+
 
 
